6b5441ea96013aa96a866c35b1b8325f7fc4bc6b
Sourcery AI <>
2023-05-12 06:20:53 +0000

    'Refactored by Sourcery'

--- bin/x86_64/linux/release/go.py
+++ bin/x86_64/linux/release/go.py
@@ -2,17 +2,11 @@ import os
 import subprocess
 for filename in os.listdir('./'):
     if os.access(filename, os.X_OK) and os.path.isfile(filename):
-        f=open("APM_%s.txt" %filename, "w")
-        print("Running %s" %filename)
-        p1=subprocess.Popen(
-           [
-               "./%s" %filename
-           ],
-           stdout=f,
-           )
-        print("Wait")
-        p1.wait()
-        print("Done")
-        f.close()
+        with open(f"APM_{filename}.txt", "w") as f:
+            print(f"Running {filename}")
+            p1 = subprocess.Popen([f"./{filename}"], stdout=f)
+            print("Wait")
+            p1.wait()
+            print("Done")
 
 
